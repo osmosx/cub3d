cd5433f9e0bd8a13343024f822ac9bbc4d478fd9
Denis Chernyavskiy <ch3rnyavskiy@yandex.ru>
2022-10-20 00:17:54 +0300

    Fix smooth

--- main.c
+++ main.c
@@ -27,8 +27,8 @@ int	main(int ac, char **av)
 	map.img.img = mlx_new_image(map.mlx_ptr, WIN_W, WIN_H);
 	map.img.data = (int *)mlx_get_data_addr(map.img.img, \
 	&map.img.bits_per_pixel, &map.line_length, &map.img.endian);
-	mlx_hook(map.win_ptr, 2, 0, &key_press, &map);
-	mlx_hook(map.win_ptr, 3, 0, &key_release, &map);
+	mlx_hook(map.win_ptr, 2, 1L << 0, &key_press, &map);
+	mlx_hook(map.win_ptr, 3, 1L << 1, &key_release, &map);
 	mlx_hook(map.win_ptr, 6, 0, mouse_move, &map);
 	mlx_hook(map.win_ptr, 17, 0, ft_exit, &map);
 	mlx_loop_hook(map.mlx_ptr, &main_loop, &map);
